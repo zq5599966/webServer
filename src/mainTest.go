1a950b126cfc9cfc1027c72a50012313ba9bf003
john <john@veewo.com>
2017-02-09 15:35:09 +0800

    websocket test

--- src/mainTest.go
+++ src/mainTest.go
@@ -1,3 +1,4 @@
+/*
 package main
 
 import (
@@ -25,5 +26,71 @@ func main() {
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
+	}
+}
+*/
+
+package main
+
+import (
+	"net/http"
+	"golang.org/x/net/websocket"
+	"log"
+	"fmt"
+	"gameProto"
+	"github.com/golang/protobuf/proto"
+)
+
+func main() {
+
+	http.Handle("/websocket", websocket.Handler(echo))
+
+	if err := http.ListenAndServe(":1234", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
+
+func echo(ws *websocket.Conn) {
+	var err error
+	for {
+		var reply []byte
+
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Printf("receive error===%v\n", err)
+			break
+		}
+
+		fmt.Println("reply======= ", reply)
+		receiveMsg(reply)
+	}
+}
+
+func receiveMsg(msg []byte) {
+	baseMsg := gameProto.BaseMessage{}
+	err := proto.Unmarshal(msg, &baseMsg)
+	if err != nil {
+		fmt.Println("unmarshal error: ", err)
+	}
+	fmt.Println("msg id======", baseMsg.GetMid())
+	fmt.Println("msg str =====", baseMsg.GetSerialization())
+
+	//msgType := gameProto.MessageType_GAME_USERINFO
+	//fmt.Println("msg type======", msgType)
+
+	switch baseMsg.GetMid() {
+	case int32(gameProto.MessageType_UNKNOWN):
+		fmt.Println("msg err")
+	case int32(gameProto.MessageType_GAME_USERINFO):
+		userInfoMsg := gameProto.GameUserInfo{}
+		err = proto.Unmarshal(baseMsg.GetSerialization(), &userInfoMsg)
+
+		fmt.Println("uid====", userInfoMsg.GetUid())
+		fmt.Println("uname====", userInfoMsg.GetUname())
+		fmt.Println("facebook id====", userInfoMsg.GetFacebookid())
+		fmt.Println("setFlagCount====", userInfoMsg.GetSetFlagCount())
+		fmt.Println("FoundFlagCount====", userInfoMsg.GetFoundFlagCount())
+	default:
+		fmt.Println("unknow msg")
 	}
 }
